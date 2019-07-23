b87fd565247a5ce6eb7cdecaefc14636827e40c2
popov7 <deniolp@gmail.com>
2019-07-23 22:57:05 +0300

    Installing prettier and configuring it with js files

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,13 @@ const AboutPage = () => {
   return (
     <div>
       <h1>About Page</h1>
-      <p>Nulla pariatur ad mollit irure. Id nostrud amet in dolore amet eiusmod non. Magna amet ea elit qui dolore reprehenderit deserunt elit mollit et. Nostrud culpa aute culpa exercitation ipsum laborum ad. Id elit occaecat duis aliquip labore non. Voluptate cillum fugiat mollit et quis duis ullamco mollit qui veniam eiusmod laboris adipisicing.</p>
+      <p>
+        Nulla pariatur ad mollit irure. Id nostrud amet in dolore amet eiusmod
+        non. Magna amet ea elit qui dolore reprehenderit deserunt elit mollit
+        et. Nostrud culpa aute culpa exercitation ipsum laborum ad. Id elit
+        occaecat duis aliquip labore non. Voluptate cillum fugiat mollit et quis
+        duis ullamco mollit qui veniam eiusmod laboris adipisicing.
+      </p>
     </div>
   );
 };
