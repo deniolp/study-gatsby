3df3837b879b7827a9a673f5a8e3b3697d372522
popov7 <deniolp@gmail.com>
2019-07-23 22:40:18 +0300

    Adding new page /about

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Nulla pariatur ad mollit irure. Id nostrud amet in dolore amet eiusmod non. Magna amet ea elit qui dolore reprehenderit deserunt elit mollit et. Nostrud culpa aute culpa exercitation ipsum laborum ad. Id elit occaecat duis aliquip labore non. Voluptate cillum fugiat mollit et quis duis ullamco mollit qui veniam eiusmod laboris adipisicing.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
