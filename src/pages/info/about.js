f52086b42b0d09f0e03168cfc20dd4531be1cd7d
popov7 <deniolp@gmail.com>
2019-07-25 21:39:06 +0300

    Installing styled-components and styling about page with it

--- src/pages/info/about.js
+++ src/pages/info/about.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import {Link} from 'gatsby';
 
-import styles from './about.module.css';
+import styled from 'styled-components';
 import Layout from '../../components/layout2';
 
 import avaImage from '../../images/Ava.jpg';
 
+const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0, auto, 12px, auto;
+`;
+
+const Avatar = styled.img`
+  flex: 0 0 96px;
+  width: 96px;
+  margin: 0;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  margin-left: 18px;
+  padding: 12px;
+`;
+
+const Username = styled.div`
+  margin: 0 0 12px 0;
+`;
+
+const Excerpt = styled.p`
+  margin: 0;
+`;
+
 const User = (props) => {
   return (
-    <div className={styles.user}>
-      <img src={props.avatar} className={styles.avatar} alt="" />
-      <div className={styles.description}>
-        <h2 className={styles.username}>{props.username}</h2>
-        <p className={styles.excerpt}>{props.excerpt}</p>
-      </div>
-    </div>
+    <UserWrapper>
+      <Avatar src={props.avatar} alt="" />
+      <Description>
+        <Username>{props.username}</Username>
+        <Excerpt>{props.excerpt}</Excerpt>
+      </Description>
+    </UserWrapper>
   );
 };
 
@@ -25,7 +51,7 @@ const AboutPage = () => (
       <User
         username="Denis Popov"
         avatar={avaImage}
-        excerpt="JSON, React and Elm developer"
+        excerpt="JS, React and Elm developer"
       />
       <br />
     </div>
