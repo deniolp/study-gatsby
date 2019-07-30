f3513054bbfd59c1b71ff78a20863fe34764ab7e
popov7 <deniolp@gmail.com>
2019-07-30 15:42:00 +0300

    Generating path from slugs and using the template for blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,7 @@
 
 // You can delete this file if you're not using it
 
+const path = require(`path`);
 const {createFilePath} = require(`gatsby-source-filesystem`);
 
 exports.onCreateNode = ({node, getNode, actions}) => {
@@ -18,4 +19,31 @@ exports.onCreateNode = ({node, getNode, actions}) => {
       value: slug,
     });
   }
+};
+
+exports.createPages = ({graphql, actions}) => {
+  const {createPage} = actions;
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug,
+        },
+      });
+    });
+  });
 };
---
+++ src/templates/blog-post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout2';
+
+export default () => {
+  return (
+    <Layout>
+      <div>Hello blog post</div>
+    </Layout>
+  );
+};
